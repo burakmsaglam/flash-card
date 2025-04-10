3bd7d72f4ad9468f48aefc6b1668aba26553c963
Burak Saglam <bsaglam5434@gmail.com>
2025-04-09 21:30:12 -0500

    feat: add dynamic background change. Still need proper css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import FlashCard from "./components/FlashCard";
-import Buttons from "./components/Button";
+import FlashCard from "./components/FlashCard/FlashCard";
+import Buttons from "./components/Button/Button";
 
 function App() {
   return (
