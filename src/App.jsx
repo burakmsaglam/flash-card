65bd00de6b52dfeee9a6d7f12722c5703d60338f
Burak Saglam <bsaglam5434@gmail.com>
2025-04-15 09:45:35 -0500

    feat: Implement card add/delete logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import "./App.css";
 import FlashCard from "./components/FlashCard/FlashCard";
-import Buttons from "./components/Button/Button";
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
 
 function App() {
+  let currentElement = 0;
+
+  const [slider, setSlider] = useState(
+    // Add number to show which card is user is on. Or do sometings else to show I dunno.
+    []
+  );
+
+  // Adds new card.
+  function handleNewCard() {
+    // From prev cards, add a new card.
+    setSlider((prevCards) => [...prevCards, <FlashCard key={nanoid()} />]);
+  }
+
+  // Deletes the current card.
+  function handleDeleteCard(e) {
+    // From prev cards, delete the current element.
+    setSlider((prevCards) => {
+      let tempSlider = [...prevCards];
+      tempSlider.splice(currentElement, 1);
+      return tempSlider;
+    });
+  }
+
   return (
     <>
-      <FlashCard />
-      <Buttons />
+      <ol>{slider}</ol>
+      <button>{"<"}</button>
+      <button>{">"}</button>
+      <button onClick={handleNewCard}>✅ Make a new card.</button>
+      <button onClick={handleDeleteCard}>🚮 Delete the current card.</button>
+      {console.log(slider)}
     </>
   );
 }
